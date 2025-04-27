5b202ea6b009a3b1c2abb35d68426664965c6c84
maheshwari9511 <120582744+maheshwari9511@users.noreply.github.com>
2025-04-27 13:36:42 +0530

    Add files via upload

---
+++ .angular/cache/17.1.2/vite/dep/@angular_fire_compat.js
@@ -0,0 +1,27 @@
+import {
+  AngularFireModule,
+  FIREBASE_APP_NAME,
+  FIREBASE_OPTIONS,
+  FirebaseApp,
+  ɵapplyMixins,
+  ɵcacheInstance,
+  ɵfirebaseAppFactory,
+  ɵlazySDKProxy
+} from "./chunk-HK5SY2JA.js";
+import "./chunk-J7TVO6EK.js";
+import "./chunk-A3TZDUC7.js";
+import "./chunk-SG3BCSKH.js";
+import "./chunk-SAVXX6OM.js";
+import "./chunk-PQ7O3X3G.js";
+import "./chunk-GLLL6ZVE.js";
+export {
+  AngularFireModule,
+  FIREBASE_APP_NAME,
+  FIREBASE_OPTIONS,
+  FirebaseApp,
+  ɵapplyMixins,
+  ɵcacheInstance,
+  ɵfirebaseAppFactory,
+  ɵlazySDKProxy
+};
+//# sourceMappingURL=@angular_fire_compat.js.map
